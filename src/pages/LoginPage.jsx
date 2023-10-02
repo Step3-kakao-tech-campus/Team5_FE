dd34147ec13cc960151897e86d1fcb989234559d
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-02 22:23:50 +0900

    feat: Implement login function
    - 렌더링시 email focus
    - 유효한 입력일 경우 로그인 api 요청

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { login } from "../apis/user";
 import Box from "../components/signup/atoms/Box";
 import Button from "../components/signup/atoms/Button";
 import Container from "../components/signup/atoms/Container";
@@ -9,65 +10,113 @@ import useInput from "../hooks/useInput";
 
 export default function LoginPage() {
   const navigate = useNavigate();
-  // eslint-disable-next-line no-unused-vars
   const [errorMessage, setErrorMessage] = useState("");
   const { values, handleChange } = useInput({
     email: "",
     password: "",
   });
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
 
-  const handleSubmit = async () => {};
+  const isValidValue = () => {
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/;
+
+    return emailRegex.test(values.email) && passwordRegex.test(values.password);
+  };
+
+  const handleLogin = async () => {
+    if (!values.email) {
+      console.log(values.email);
+      setErrorMessage("이메일을 입력해주세요.");
+      emailInputRef.current.focus();
+      return;
+    }
+    if (!values.password) {
+      setErrorMessage("비밀번호를 입력해주세요.");
+      passwordInputRef.current.focus();
+      return;
+    }
+    if (!isValidValue()) {
+      // validation check fail시 api 호출하지 않음
+      setErrorMessage("이메일 또는 비밀번호를 잘못 입력했습니다. ");
+      return;
+    }
+    try {
+      await login({
+        email: values.email,
+        password: values.password,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("이메일 또는 비밀번호를 잘못 입력했습니다. ");
+    }
+  };
+
+  // 첫 렌더링 시 email input에 focus
+  useEffect(() => {
+    if (emailInputRef.current) {
+      emailInputRef.current.focus();
+    }
+  }, []);
 
   return (
     <Container className="max-w-none">
       <Box className="relative h-full mx-auto px-[29px] pt-[45px] text-xs justify-center">
-        <InputGroup
-          id="email"
-          type="email"
-          name="email"
-          label="이메일"
-          placeholder="이메일"
-          value={values.email}
-          onChange={handleChange}
-          className="relative pt-[15px]"
-        />
-        <InputGroup
-          id="password"
-          type="password"
-          name="password"
-          label="비밀번호"
-          placeholder="비밀번호"
-          value={values.password}
-          onChange={handleChange}
-          className="relative pt-[15px]"
-        />
-        {errorMessage && (
-          <AlertBox
-            id="errorMessage"
-            className="mt-[10px] pl-[20px] py-[15px] text-xs rounded-[10px] border"
-            label={errorMessage}
+        <form>
+          <InputGroup
+            id="email"
+            type="email"
+            name="email"
+            label="이메일"
+            placeholder="이메일"
+            value={values.email}
+            onChange={handleChange}
+            className="relative pt-[15px]"
+            ref={emailInputRef}
           />
-        )}
-        <Button
-          onClick={() => {
-            handleSubmit();
-          }}
-          className="block w-full h-[50px] mt-[30px] rounded-[10px] font-normal text-sm bg-lightskyblue-sunsu"
-        >
-          로그인
-        </Button>
-        <div className="flex items-center justify-center pt-5 tracking-tight gap-2">
-          <span>아직 계정이 없으신가요?</span>
-          <button
-            className=" underline font-bold"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/signup");
+          <InputGroup
+            id="password"
+            type="password"
+            name="password"
+            label="비밀번호"
+            placeholder="비밀번호"
+            value={values.password}
+            onChange={handleChange}
+            className="relative pt-[15px]"
+            ref={passwordInputRef}
+          />
+          {errorMessage && (
+            <AlertBox
+              id="errorMessage"
+              className="mt-[10px] pl-[20px] py-[15px] text-xs rounded-[10px] border"
+              label={errorMessage}
+            />
+          )}
+          <Button
+            onClick={() => {
+              handleLogin();
             }}
+            className="block w-full h-[50px] mt-[30px] rounded-[10px] font-normal text-sm bg-lightskyblue-sunsu"
           >
-            회원가입
-          </button>
-        </div>
+            로그인
+          </Button>
+          <div className="flex items-center justify-center pt-5 tracking-tight gap-2">
+            <span>아직 계정이 없으신가요?</span>
+            <button
+              className=" underline font-bold"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("/signup");
+              }}
+            >
+              회원가입
+            </button>
+          </div>
+        </form>
       </Box>
     </Container>
   );
