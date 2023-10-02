97f6aeee9839b31d117333e2b56cc9af216ad625
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-02 22:25:37 +0900

    feat: Make reference available

--- src/components/signup/atoms/Input.jsx
+++ src/components/signup/atoms/Input.jsx
@@ -1,23 +1,20 @@
-const Input = ({
-  id,
-  type,
-  name,
-  value,
-  placeholder,
-  className = "",
-  onChange,
-}) => {
-  return (
-    <input
-      id={id}
-      name={name}
-      className={`input ${className}`}
-      type={type}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-    />
-  );
-};
+import React, { forwardRef } from "react";
+
+const Input = forwardRef(
+  ({ id, type, name, value, placeholder, className = "", onChange }, ref) => {
+    return (
+      <input
+        id={id}
+        name={name}
+        className={`input ${className}`}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        ref={ref}
+      />
+    );
+  }
+);
 
 export default Input;
