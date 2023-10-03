be5b28107505f7cde3f7eae9dbaa42fc7294efdf
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-03 16:15:51 +0900

    feat: Implement ProfilePage layout

--- src/components/profile/ProfileTemplate.jsx
+++ src/components/profile/ProfileTemplate.jsx
@@ -1,15 +1,18 @@
 import CircularProgress from "@mui/material/CircularProgress";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useQuery } from "react-query";
 import { getUserInfo } from "../../apis/user";
+import Modal from "../common/Modal";
 
 export default function ProfileTemplate() {
   const { data, isLoading } = useQuery("/user/info", getUserInfo);
+  const [modalOpen, setModalOpen] = useState(false);
   const userInfo = data?.response;
   useEffect(() => {
     console.log(data);
   }, [data]);
-
+  const modalRef = useRef(null);
+  console.log(modalRef.current);
   if (isLoading)
     return (
       <div className="flex justify-center h-1/5 items-center">
@@ -19,24 +22,35 @@ export default function ProfileTemplate() {
         />
       </div>
     );
-
   return (
-    <div>
-      {/* 유저 정보 영역 */}
-      <div className="flex flex-col">
-        <span>안녕하세요</span>
-        <span>{userInfo.username}님</span>
-        <span>{userInfo.email}</span>
-      </div>
-      {/* 결제 영역 */}
-      <div className="flex flex-col">
-        <span>서비스</span>
-        <button>순수 멤버십</button>
-      </div>
-      {/* 로그아웃 & 회원탈퇴 */}
-      <div className="flex flex-col">
-        <span>로그아웃</span>
-        <span>회원탈퇴</span>
+    <div className="w-full h-full relative ">
+      {modalOpen && (
+        <Modal
+          handler={() => {
+            setModalOpen(false);
+          }}
+        />
+      )}
+      <div className="w-full h-full relative p-16 flex flex-col gap-10">
+        {/* 유저 정보 영역 */}
+        <div className="flex flex-col pt-10">
+          <span className=" font-medium text-3xl">안녕하세요</span>
+          <span className=" font-medium text-3xl">{userInfo.username}님</span>
+          <span className=" tracking-tighter pt-2">{userInfo.email}</span>
+        </div>
+        {/* 결제 영역 */}
+        <div className="flex flex-col text-2xl gap-2">
+          <span className="text-xl text-zinc-500">서비스</span>
+          <button className=" w-fit" onClick={() => setModalOpen(true)}>
+            순수 멤버십
+          </button>
+        </div>
+        {/* 로그아웃 & 회원탈퇴 */}
+        <div className="flex flex-col text-2xl gap-2">
+          <span className="text-xl text-zinc-500">회원정보</span>
+          <span>로그아웃</span>
+          <span>회원탈퇴</span>
+        </div>
       </div>
     </div>
   );
