c91ae82699371c543bc086c7ab4890422f1fc00c
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-03 16:55:26 +0900

    feat: Divide user by login status

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import GNBBOX from "../components/common/GNBBOX";
 import ProfileTemplate from "../components/profile/ProfileTemplate";
+import RequiredLoginTemplate from "../components/profile/RequiredLoginTemplate";
 
 export default function ProfilePage() {
+  const userToken = localStorage.getItem("token");
   return (
     <div className="flex h-full flex-col">
       <div className="w-full h-full">
-        <ProfileTemplate />
+        {userToken ? <ProfileTemplate /> : <RequiredLoginTemplate />}
       </div>
       <GNBBOX />
     </div>
