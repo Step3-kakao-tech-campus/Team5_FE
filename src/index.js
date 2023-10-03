97c4e4605e9c490f379dcce7484c85824b6bb531
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-03 20:02:10 +0900

    feat: Create global store by redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClientProvider, QueryClient } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { persistor, store } from "./store";
 
 if (process.env.NODE_ENV === "development") {
   // eslint-disable-next-line global-require
@@ -16,9 +19,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
