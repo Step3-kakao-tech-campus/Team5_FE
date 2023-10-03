6fa96b9429dd892b08f4d53ff0817b61f5cea851
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-03 16:13:07 +0900

    chore: Change self-closing-tag

--- src/components/common/GNBBOX.jsx
+++ src/components/common/GNBBOX.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function GNBBOX() {
-  return <div className="w-full max-w-[768px] h-[50px]">GNBBOX</div>;
+  return <div className="w-full max-w-[768px] h-[50px]" />;
 }
