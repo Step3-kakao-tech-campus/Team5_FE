7b0c5b89e36420ad89ff93af024855feb1859071
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-03 16:56:52 +0900

    feat: Implement logout function

--- src/components/profile/ProfileTemplate.jsx
+++ src/components/profile/ProfileTemplate.jsx
@@ -1,18 +1,19 @@
 import CircularProgress from "@mui/material/CircularProgress";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import { getUserInfo } from "../../apis/user";
-import Modal from "../common/Modal";
+import PaymentModal from "./PaymentModal";
 
 export default function ProfileTemplate() {
   const { data, isLoading } = useQuery("/user/info", getUserInfo);
   const [modalOpen, setModalOpen] = useState(false);
   const userInfo = data?.response;
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
-  const modalRef = useRef(null);
-  console.log(modalRef.current);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
   if (isLoading)
     return (
       <div className="flex justify-center h-1/5 items-center">
@@ -24,17 +25,11 @@ export default function ProfileTemplate() {
     );
   return (
     <div className="w-full h-full relative ">
-      {modalOpen && (
-        <Modal
-          handler={() => {
-            setModalOpen(false);
-          }}
-        />
-      )}
+      {modalOpen && <PaymentModal handler={() => setModalOpen(false)} />}
       <div className="w-full h-full relative p-16 flex flex-col gap-10">
         {/* 유저 정보 영역 */}
         <div className="flex flex-col pt-10">
-          <span className=" font-medium text-3xl">안녕하세요</span>
+          <span className=" font-normal text-3xl">안녕하세요</span>
           <span className=" font-medium text-3xl">{userInfo.username}님</span>
           <span className=" tracking-tighter pt-2">{userInfo.email}</span>
         </div>
@@ -48,8 +43,15 @@ export default function ProfileTemplate() {
         {/* 로그아웃 & 회원탈퇴 */}
         <div className="flex flex-col text-2xl gap-2">
           <span className="text-xl text-zinc-500">회원정보</span>
-          <span>로그아웃</span>
-          <span>회원탈퇴</span>
+          <button
+            className=" w-fit"
+            onClick={() => {
+              handleLogout();
+            }}
+          >
+            로그아웃
+          </button>
+          <button className=" w-fit">회원탈퇴</button>
         </div>
       </div>
     </div>
