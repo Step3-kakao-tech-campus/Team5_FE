0cfef762fa5dc8a4f2065844b386a2e87cfe8922
blackhblee <blackhblee@gmail.com>
2023-10-09 02:00:47 +0900

    design: Fix GNB sticky

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,10 +4,16 @@ import GNB from "../components/common/GNB";
 
 export default function MainLayout() {
   return (
-    <div className=" w-screen flex justify-center bg-zinc-50">
-      <div className=" max-w-[576px] w-full h-full bg-white relative">
-        <Outlet />
-        <GNB />
+    <div className="flex flex-col min-h-screen bg-zinc-50">
+      <div className="flex-grow flex justify-center">
+        <div className="max-w-[576px] w-full bg-white relative">
+          <Outlet />
+        </div>
+      </div>
+      <div className="flex justify-center sticky bottom-0">
+        <div className="max-w-[576px] w-full">
+          <GNB />
+        </div>
       </div>
     </div>
   );
