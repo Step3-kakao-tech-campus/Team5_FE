5d22da4ae067593b815f372f551f8e5bd637d1df
blackhblee <blackhblee@gmail.com>
2023-10-10 22:53:52 +0900

    chore: Change mock data

--- src/mocks/responseData.js
+++ src/mocks/responseData.js
@@ -11,7 +11,7 @@ export const userInfo = {
     username: "김하나",
     email: "hana0101@naver.com",
     role: "couple",
-    grade: "normal",
+    grade: "premium",
     payedAt: "2023년 09월 15일",
   },
   error: null,
@@ -23,38 +23,38 @@ export const portfolioList1 = {
     {
       id: 1, // Long
       image: "/images/1.jpg",
-      title: "한 번 뿐인 결혼, 가장 아름답게 만들어드리겠습니다.",
-      plannerName: "유희정1",
-      price: 1110000,
+      title: "최고의 결혼을 만들어주는 이 시대의 웨딩플래너 “유희정”입니다.",
+      plannerName: "유희정",
+      price: 3400000,
       location: "서울",
-      contractCount: 1,
+      contractCount: 225,
     },
     {
       id: 2, // Long
       image: "/images/2.jpg",
-      title: "한 번 뿐인 결혼, 가장 아름답게 만들어드리겠습니다.",
-      plannerName: "유희정2",
-      price: 2220000,
+      title: "한 번 뿐인 결혼, 아름답게 만들어드리겠습니다.",
+      plannerName: "김아름",
+      price: 2950000,
       location: "서울",
-      contractCount: 2,
+      contractCount: 21,
     },
     {
       id: 3, // Long
       image: "/images/3.jpg",
-      title: "한 번 뿐인 결혼, 가장 아름답게 만들어드리겠습니다.",
-      plannerName: "유희정3",
-      price: 3330000,
+      title: "소중한 날 가장 아름다운 두 사람이 될 수 있도록",
+      plannerName: "백도희",
+      price: 3100000,
       location: "서울",
-      contractCount: 3,
+      contractCount: 95,
     },
     {
       id: 4, // Long
       image: "/images/4.jpg",
-      title: "한 번 뿐인 결혼, 가장 아름답게 만들어드리겠습니다.",
-      plannerName: "유희정4",
-      price: 4440000,
+      title: "꿈꿔왔던 순간, 진심으로 함께할 플래너",
+      plannerName: "임혜주",
+      price: 3210000,
       location: "서울",
-      contractCount: 4,
+      contractCount: 83,
     },
     {
       id: 5, // Long
@@ -314,65 +314,66 @@ export const portfolioDetail = {
     id: 1, // Long
     userId: 999,
     images: ["/images/1.jpg", "/images/2.jpg", "/images/3.jpg"],
-    title: "한 번 뿐인 결혼, 가장 아름답게 만들어드리겠습니다.",
+    title: "최고의 결혼을 만들어주는 이 시대의 웨딩플래너 “유희정”입니다.",
     plannerName: "유희정",
-    contractCount: 10, // 추가
+    contractCount: 225, // 추가
     priceInfo: {
-      totalPrice: 3000000, // 플래너가 제시한 가격
+      totalPrice: 3400000, // 플래너가 제시한 가격
       items: [
         {
-          itemTitle: "헤어",
-          itemPrice: 3000000,
-        },
-        {
           itemTitle: "스튜디오",
-          itemPrice: 3000000,
+          itemPrice: 2000000,
         },
         {
-          itemTitle: "메이크업",
-          itemPrice: 3000000,
+          itemTitle: "드레스",
+          itemPrice: 1450000,
+        },
+        {
+          itemTitle: "헤어/메이크업",
+          itemPrice: 950000,
         },
       ], // priceItem ended
     }, // price ended
     location: "서울",
-    description: "긴 소개글",
-    career: "☝️ 경력\n 웨딩 플래너 - 행복 웨딩홀 (2019년 - 2022년)",
+    description:
+      "안녕하세요, 이 시대 최고의 웨딩플래너 “유희정”입니다. 수많은 커플들의 특별한 순간을 함께 만들어온 전문가로, 감동과 아름다움을 실현해드립니다. 세심한 계획과 창의적인 아이디어로 맞춤형 웨딩을 완성하며, 최상의 서비스와 행복한 추억을 고객에게 선사합니다. 눈부신 순간들로 가득한 특별한 하루를 함께 계획하고 싶다면, 저와 함께하는 결정은 언제나 환영입니다. 👰🤵‍♂️",
+    career: "2019 - 2022 : 웨딩플래너 - 행복 웨딩홀",
     partnerCompany:
-      "✌️주요 제휴 업체\n 섬머 플로리스트🌸: 웨딩 꽃장식을 위해 협력한 플로리스트로서 세심한 디자인과 고품질의 꽃을 제공해 웨딩 분위기를 더욱 화려하게 만들어주었습니다.",
+      "1. 섬머 플로리스트🌸: 웨딩 꽃장식을 위해 협력한 플로리스트로서 세심한 디자인과 고품질의 꽃을 제공해 웨딩 분위기를 더욱 화려하게 만들어주었습니다.\n2. 피노 케이터링🍰: 피노 케이터링과의 협업을 통해 탁월한 음식과 서비스를 웨딩 행사에 제공하여 게스트의 입맛을 사로잡았습니다.\n3. 릴리 스튜디오📷: 웨딩 촬영 추천으로 소문이 나있는 릴리 스튜디오는 촬영이 어색한 사람들에게 분위기를 유도해주는 포토그래퍼가 있어 더욱 자연스러운 사진을 얻을 수 있습니다.",
     paymentHistory: {
       // mvp아님 // normal일땐 null
-      avgPrice: 2000000,
-      minPrice: 1000000,
-      maxPrice: 3000000,
+      avgPrice: 3507000,
+      minPrice: 2950000,
+      maxPrice: 4170000,
       payments: [
         {
-          price: 3000000,
-          confirmedAt: "2023.09", // 월까지만 기입(확정하기를 누른 날짜)
+          price: 3850000,
+          confirmedAt: "2023.08", // 월까지만 기입(확정하기를 누른 날짜)
           paymentItems: [
             // 최대 10개
             {
-              paymentTitle: "헤어",
-              paymentPrice: 3000000,
-              paymentCompany: "",
-              paymentDescription: "",
+              paymentTitle: "스튜디오",
+              paymentPrice: 1950000,
+              paymentCompany: "릴스튜디오",
+              paymentDescription: "원데이 촬영, 원본 구매 및 보정",
             },
             {
-              paymentTitle: "헤어",
-              paymentPrice: 3000000,
-              paymentCompany: "",
-              paymentDescription: "",
+              paymentTitle: "드레스",
+              paymentPrice: 1900000,
+              paymentCompany: "르셀린",
+              paymentDescription: "촬영+본식, 드레스 4벌",
             },
             {
-              paymentTitle: "헤어",
-              paymentPrice: 3000000,
-              paymentCompany: "",
-              paymentDescription: "",
+              paymentTitle: "헤어/메이크업",
+              paymentPrice: 1000000,
+              paymentCompany: "에브뉴청담",
+              paymentDescription: "촬영+본식, 헤어메이크업 원장",
             },
           ],
         },
         {
           price: 3000000,
-          confirmedAt: "2023.09", // 월까지만 기입
+          confirmedAt: "2023.08", // 월까지만 기입
           paymentItems: [
             // 최대 10개
             {
