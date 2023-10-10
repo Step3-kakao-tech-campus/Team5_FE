92ee5be5fec02377588d0b4c307427c04955bdbd
blackhblee <blackhblee@gmail.com>
2023-10-11 00:05:06 +0900

    feat: Implement spinner
    스피너를 구현했습니다.

---
+++ src/components/common/atoms/Spinner.jsx
@@ -0,0 +1,11 @@
+import { ReactComponent as SpinnerIcon } from "../../../assets/spinner-01.svg";
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <SpinnerIcon />
+    </div>
+  );
+};
+
+export default Spinner;
