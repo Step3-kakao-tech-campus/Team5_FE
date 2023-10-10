1065d2c859bfecbb9bd892c43927ee181a99ec65
blackhblee <blackhblee@gmail.com>
2023-10-10 22:54:33 +0900

    design: Fix bottomsheet design
    모달창이 정위치에 구현이 되지 않았던 점을 수정했습니다.

--- src/components/common/BottomSheet.jsx
+++ src/components/common/BottomSheet.jsx
@@ -12,9 +12,12 @@ export default function BottomSheet({ handler, children }) {
     <>
       <div
         ref={modalWrapperRef}
-        className="modal_wrapper w-full h-full bg-black flex items-center justify-center absolute opacity-30 z-10"
+        className="modal_wrapper w-full max-w-[576px] h-full bg-black flex items-center justify-center fixed opacity-50 z-10"
       />
-      <div ref={modalRef} className="absolute bottom-0 w-full z-10">
+      <div
+        ref={modalRef}
+        className="fixed max-w-[576px] bottom-0 w-full z-10 mb-[50px]"
+      >
         {/* 모달 상단 라운드 영역 */}
         <div className="bg-white w-full rounded-t-full h-8 flex flex-row-reverse">
           <button onClick={handler}>
@@ -22,7 +25,7 @@ export default function BottomSheet({ handler, children }) {
           </button>
         </div>
         {/* 모달 내용 */}
-        <div className="bg-white px-[38px] pt-0.5 pb-[35px]">{children}</div>
+        <div className="bg-white px-[30px] pt-0.5 pb-[35px]">{children}</div>
       </div>
     </>
   );
