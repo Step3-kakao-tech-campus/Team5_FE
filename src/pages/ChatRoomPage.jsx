98d494a141d2823259afbd881669c1bf313fc1f8
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-12 16:59:42 +0900

    feat: Create ChatInput component

---
+++ src/pages/ChatRoomPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChatInput from "../components/chatroom/ChatInput";
+
+export default function ChatRoomPage() {
+  return (
+    <div>
+      <ChatInput />
+    </div>
+  );
+}
