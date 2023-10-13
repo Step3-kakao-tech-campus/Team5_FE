e0dbe1aa44c682af0b3517b5af9cc1cc4a2e8277
blackhblee <blackhblee@gmail.com>
2023-10-13 14:36:06 +0900

    design: Improve color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
     extend: {
       colors: {
         "lightskyblue-sunsu": "#A7CFFF",
-        "skyblue-sunsu": "#7AA6DC",
+        "skyblue-sunsu": "#4299EC",
         "blue-sunsu": "#0073C2",
         "lightgray-sunsu": "#D9D9D9",
-        "gray-sunsu": "#666666",
+        "gray-sunsu": "#667788",
         "darkgray-sunsu": "#292D32",
         "red-sunsu": "#EC6A5E",
       },
