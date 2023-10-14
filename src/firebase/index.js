d0cb45cec2c35c8a4fc1ecc0aa6719d5194b7863
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-15 06:57:44 +0900

    chore: Add gitignore

---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkAwRwhT7rY1l07YjlQDNg67eSMOTJ-fg",
+  authDomain: "sunsuwedding-chat.firebaseapp.com",
+  databaseURL:
+    "https://sunsuwedding-chat-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sunsuwedding-chat",
+  storageBucket: "sunsuwedding-chat.appspot.com",
+  messagingSenderId: "534295753892",
+  appId: "1:534295753892:web:9a3200fb94aa00cc25e231",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
