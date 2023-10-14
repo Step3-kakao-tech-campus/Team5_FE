e81e7acec1f22039186aecb3fa2b6cf9b09e83bf
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-15 05:51:01 +0900

    chore: Add date convert function in utils

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 export const comma = (num) => {
   // Truthy, Falsy로만 판별하면 type을 검사하지 못함
   if (num === undefined || num === null) {
@@ -12,4 +14,21 @@ export const comma = (num) => {
     num = parseInt(num);
   }
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+// timestamp를 받아서 "2021년 10월 10일 일요일" 형식으로 포맷팅하는 함수
+export const convertToDate = (timestamp) => {
+  const daysOfWeek = [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+  ];
+  const date = dayjs(timestamp);
+  const formattedDate =
+    date.format("YYYY년 MM월 DD일 ") + daysOfWeek[date.day()];
+  return formattedDate;
 };
