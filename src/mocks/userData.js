4cc7b8b40633d68b49bbf8d01d8014a7760d4a72
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-14 19:18:17 +0900

    feat: Implement virtual user

--- src/mocks/userData.js
+++ src/mocks/userData.js
@@ -14,15 +14,54 @@ export const signupResponse = {
 // SuccessResponse
 
 // /user/info
-export const infoResponse = {
+export const infoResponseHJ = {
   success: true,
   response: {
     userId: 1,
+    username: "유희정",
+    email: "hj1@naver.com",
+    role: "planner",
+    grade: "premium",
+    payedAt: "2023년 09월 15일",
+  },
+  error: null,
+};
+
+export const infoResponseAR = {
+  success: true,
+  response: {
+    userId: 2,
+    username: "김아름",
+    email: "ar2@naver.com",
+    role: "planner",
+    grade: "normal",
+    payedAt: null,
+  },
+  error: null,
+};
+
+export const infoResponseHN = {
+  success: true,
+  response: {
+    userId: 1001,
     username: "김하나",
-    email: "hana0101@naver.com",
+    email: "hn1001@naver.com",
     role: "couple",
     grade: "premium",
     payedAt: "2023년 09월 15일",
+  },
+  error: null,
+};
+
+export const infoResponseDH = {
+  success: true,
+  response: {
+    userId: 1002,
+    username: "백도희",
+    email: "dh1002@naver.com",
+    role: "couple",
+    grade: "normal",
+    payedAt: null,
   },
   error: null,
 };
--- src/mocks/userHandler.js
+++ src/mocks/userHandler.js
@@ -1,6 +1,12 @@
 import { rest } from "msw";
 import { sucess } from "./commonData";
-import { infoResponse, signupResponse } from "./userData";
+import {
+  infoResponseAR,
+  infoResponseDH,
+  infoResponseHJ,
+  infoResponseHN,
+  signupResponse,
+} from "./userData";
 
 async function sleep(ms) {
   return new Promise((resolve) => {
@@ -17,14 +23,37 @@ export const userHandlers = [
 
   // /user/login
   rest.post("/user/login", async (req, res, ctx) => {
-    const fakeToken = "SunsuWeddingToken";
-    console.log(req.body);
-
     await sleep(500);
+    const hjToken = "1";
+    const arToken = "2";
+    const hnToken = "1001";
+    const dhToken = "1002";
+    if (req.body.email === "hj1@naver.com") {
+      return res(
+        ctx.status(200),
+        ctx.json(sucess),
+        ctx.set("Authorization", hjToken),
+      );
+    }
+    if (req.body.email === "ar2@naver.com") {
+      return res(
+        ctx.status(200),
+        ctx.json(sucess),
+        ctx.set("Authorization", arToken),
+      );
+    }
+    if (req.body.email === "hn1001@naver.com") {
+      return res(
+        ctx.status(200),
+        ctx.json(sucess),
+        ctx.set("Authorization", hnToken),
+      );
+    }
+
     return res(
       ctx.status(200),
       ctx.json(sucess),
-      ctx.set("Authorization", `Bearer ${fakeToken}`),
+      ctx.set("Authorization", dhToken),
     );
   }),
 
@@ -57,7 +86,16 @@ export const userHandlers = [
         }),
       );
     }
-    return res(ctx.status(200), ctx.json(infoResponse));
+    if (isAuthenticated === "1") {
+      return res(ctx.status(200), ctx.json(infoResponseHJ));
+    }
+    if (isAuthenticated === "2") {
+      return res(ctx.status(200), ctx.json(infoResponseAR));
+    }
+    if (isAuthenticated === "1001") {
+      return res(ctx.status(200), ctx.json(infoResponseHN));
+    }
+    return res(ctx.status(200), ctx.json(infoResponseDH));
   }),
 
   // /user/upgrade
