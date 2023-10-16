5ce99852951b20dd4ca5013896b14e32d1d87c52
blackhblee <blackhblee@gmail.com>
2023-10-16 23:57:35 +0900

    design: Set line-height to 1.55

--- src/components/portfolios/DescriptionRow.jsx
+++ src/components/portfolios/DescriptionRow.jsx
@@ -2,7 +2,7 @@ const DescriptionRow = ({ title, detail }) => {
   return (
     <div className="justify-between p-5 whitespace-pre-line">
       <div className="text-base font-bold">{title}</div>
-      <div className="text-sm pt-2.5">{detail}</div>
+      <div className="text-sm pt-2.5 leading-[1.55]">{detail}</div>
     </div>
   );
 };
