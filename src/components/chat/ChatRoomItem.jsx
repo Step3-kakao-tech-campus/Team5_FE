52be22c2d458799878bd395b10e3aa8e254e9a0b
blackhblee <blackhblee@gmail.com>
2023-10-17 01:00:47 +0900

    design: Fix padding value

--- src/components/chat/ChatRoomItem.jsx
+++ src/components/chat/ChatRoomItem.jsx
@@ -13,23 +13,23 @@ export default function ChatRoomItem({
 
   return (
     <button
-      className="p-3 flex flex-col w-full"
+      className="px-[29px] pt-[15px] pb-[20px] flex flex-col w-full"
       onClick={() => {
         navigate(`/chat/${chatId}`);
       }}
     >
       <div className="flex justify-between items-end w-full">
         <span className="font-bold text-lg">{counterName}</span>
-        <span className=" text-sm text-zinc-500">
+        <span className="text-sm text-zinc-500">
           {dayjs(timestamp).format("YYYY.MM.DD")}
         </span>
       </div>
-      <div className=" h-8 flex justify-between w-full items-end">
+      <div className="h-8 flex justify-between w-full items-end">
         <span className="text-ellipsis whitespace-nowrap overflow-hidden w-4/5 text-base text-left">
           {lastMessage}
         </span>
         {unreadCount > 0 && (
-          <span className=" text-xs bg-red-500 text-white rounded-full self-start px-2">
+          <span className="text-xs bg-red-500 text-white rounded-full self-start px-2">
             {unreadCount}
           </span>
         )}
