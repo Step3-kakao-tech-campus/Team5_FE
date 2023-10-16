2c8521577d002aac3c1a6ad7197a949c30fc8c1c
blackhblee <blackhblee@gmail.com>
2023-10-16 23:53:43 +0900

    feat: Add header row

---
+++ src/components/main/MainHeaderRow.jsx
@@ -0,0 +1,9 @@
+const MainHeaderRow = () => {
+  return (
+    <div className="header-row text-xl flex w-full h-[50px]">
+      <div className="my-auto ml-5">Sunsu</div>
+    </div>
+  );
+};
+
+export default MainHeaderRow;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import GNBBOX from "../components/common/GNBBOX";
 import MainCarousel from "../components/main/MainCarousel";
+import MainHeaderRow from "../components/main/MainHeaderRow";
 
 export default function MainPage() {
   return (
     <div className="flex w-full h-full flex-col">
-      <div className="w-full h-full overflow-x-hidden">
+      <div className="w-full h-full overflow-x-hidden overflow-y-auto">
+        <MainHeaderRow />
         <MainCarousel />
       </div>
       <GNBBOX />
