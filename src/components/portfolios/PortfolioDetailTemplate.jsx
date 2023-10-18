2d6b07ee957e39fd00f6f5ba35134507187a9051
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-19 02:32:20 +0900

    style: Add hover effect on payment button

--- src/components/portfolios/PortfolioDetailTemplate.jsx
+++ src/components/portfolios/PortfolioDetailTemplate.jsx
@@ -179,16 +179,17 @@ const PortfolioDetailTemplate = ({ portfolio }) => {
               </div>
             </>
           ) : (
-            <div className="flex h-[46px]">
-              <div className="inline text-sm mr-[10px] break-keep">
-                이전 매칭 내역을 확인하려면 순수 멤버십을 결제하셔야 합니다.
+            <div className="flex h-[46px] justify-between">
+              <div className="text-sm mr-[10px] flex flex-col">
+                <span>이전 매칭 내역을 확인하려면</span>
+                <span>순수 멤버십을 결제하셔야 합니다.</span>
               </div>
               <div className="inline w-[120px]">
                 <Button
                   onClick={() => {
                     setPaymentBottomSheetOpen(true);
                   }}
-                  className="block w-full h-full rounded-[10px] font-normal text-sm bg-blue-sunsu text-white"
+                  className="block w-full h-full rounded-[10px] font-normal text-sm text-black border-solid border-skyblue-sunsu border-2 hover:bg-blue-sunsu hover:text-white"
                 >
                   결제하기
                 </Button>
