b2b76a78edc0011be0ddc18507f4858aa56ae375
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-21 06:34:44 +0900

    style: Add search icon at MainHeaderRow

--- src/components/main/MainHeaderRow.jsx
+++ src/components/main/MainHeaderRow.jsx
@@ -1,9 +1,18 @@
+import { ReactComponent as SearchOutlinedIcon } from "../../assets/search-01.svg";
 import HeaderRow from "../common/HeaderRow";
 
-const MainHeaderRow = () => {
+const MainHeaderRow = ({ handleOpenSearchBar }) => {
   return (
     <HeaderRow>
-      <div className="my-auto font-serif">Sunsu</div>
+      <div className="flex justify-between items-center w-full px-3">
+        <div className="my-auto font-serif">Sunsu</div>
+        <button onClick={() => {}}>
+          <SearchOutlinedIcon
+            className="w-4 h-4"
+            onClick={handleOpenSearchBar}
+          />
+        </button>
+      </div>
     </HeaderRow>
   );
 };
