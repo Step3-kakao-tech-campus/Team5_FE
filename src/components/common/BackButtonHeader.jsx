177adaf0210ad137c8d1552a89ad4a93a430904d
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-21 02:17:10 +0900

    feat: Create PortfolioDetailHeader component

--- src/components/common/BackButtonHeader.jsx
+++ src/components/common/BackButtonHeader.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 export default function BackButtonHeader({ children }) {
   const navigate = useNavigate();
   return (
-    <div className=" border-solid border-0 border-b-2 h-10 border-zinc-200 flex items-center gap-2 fixed z-10 top-0 w-full max-w-[576px] bg-white">
+    <div className=" border-b h-9 border-zinc-200 flex items-center gap-2 sticky z-10 top-0 bg-white">
       <IoChevronBack
         className="text-2xl cursor-pointer absolute left-2"
         onClick={() => {
