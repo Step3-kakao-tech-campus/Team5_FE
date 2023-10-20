4b1a6d77cb26234d00154cdd56a729626be9f81c
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-21 05:00:27 +0900

    style: Set higher z-index at BottomSheet

--- src/components/common/bottomsheet/BottomSheet.jsx
+++ src/components/common/bottomsheet/BottomSheet.jsx
@@ -12,9 +12,9 @@ export default function BottomSheet({ onClose, children }) {
     <>
       <div
         ref={modalWrapperRef}
-        className="modal_wrapper w-full max-w-[576px] h-full bg-black flex items-center justify-center fixed opacity-50 z-10"
+        className="modal_wrapper w-full max-w-[576px] h-full bg-black flex items-center justify-center fixed opacity-50 z-20"
       />
-      <div ref={modalRef} className="fixed max-w-[576px] bottom-0 w-full z-10">
+      <div ref={modalRef} className="fixed max-w-[576px] bottom-0 w-full z-20">
         {/* 모달 상단 라운드 영역 */}
         <div className="bg-white w-full rounded-t-full h-8 flex flex-row-reverse">
           <button onClick={onClose}>
