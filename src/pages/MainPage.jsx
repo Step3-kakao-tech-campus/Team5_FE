55fb07498e0bc3bc0ddbaf0065bcffeaeb5f955e
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-21 06:35:56 +0900

    feat: Add SearchBar in MainPage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import GNBBOX from "../components/common/GNBBOX";
 import MainCarousel from "../components/main/MainCarousel";
 import MainHeaderRow from "../components/main/MainHeaderRow";
+import SearchBar from "../components/common/SearchBar";
 
 export default function MainPage() {
+  const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
+
+  const handleOpenSearchBar = () => {
+    setIsSearchBarOpen(true);
+  };
+  const handleCloseSearchBar = () => {
+    setIsSearchBarOpen(false);
+  };
+
   return (
     <div className="flex w-full h-full flex-col">
       <div className="w-full h-full ">
-        <MainHeaderRow />
+        {isSearchBarOpen && (
+          <SearchBar handleCloseSearchBar={handleCloseSearchBar} />
+        )}
+        {!isSearchBarOpen && (
+          <MainHeaderRow handleOpenSearchBar={handleOpenSearchBar} />
+        )}
         <MainCarousel />
       </div>
       <GNBBOX />
