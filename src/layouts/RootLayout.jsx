b45471312905a361358a5a5f5a2740d9e6386b98
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-21 16:58:23 +0900

    Fix: Resolve width bugs by scrollbar differences in Mac and Windows

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,7 +4,7 @@ import GlobalBottomSheet from "../components/common/bottomsheet/GlobalBottomShee
 
 export default function RootLayout() {
   return (
-    <div className="w-screen flex justify-center bg-blue-50">
+    <div className=" flex justify-center bg-blue-50 w-full ">
       <div className="max-w-[576px] min-h-screen w-full h-full bg-white relative">
         <GlobalBottomSheet />
         <Outlet />
