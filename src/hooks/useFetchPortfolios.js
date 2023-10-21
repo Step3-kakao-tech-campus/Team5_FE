3df1e3a7a5456b0063ac07f561dde39cdba83e85
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-21 23:16:40 +0900

    feat: Add dependency parameters on useFetchPortfolios for filtering

--- src/hooks/useFetchPortfolios.js
+++ src/hooks/useFetchPortfolios.js
@@ -8,7 +8,8 @@ export default function useFetchPortfolios({
   maxPrice,
 }) {
   const infiniteQuery = useInfiniteQuery(
-    ["portfolios"],
+    // 의존성 배열 (쿼리 키)
+    ["portfolios", name, location, minPrice, maxPrice],
     ({ pageParam = -1 }) =>
       getPortfolioList(pageParam, name, location, minPrice, maxPrice),
     {
