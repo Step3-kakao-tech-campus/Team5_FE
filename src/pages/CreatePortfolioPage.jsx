9c02cba44fcebbe17e6ddfdc5e93e6fcd4c2657f
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-31 03:17:18 +0900

    feat: Add spinner when CreatePortfolio loading

--- src/pages/CreatePortfolioPage.jsx
+++ src/pages/CreatePortfolioPage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { getPortfolioSelf } from "../apis/portfolio";
+import Spinner from "../components/common/atoms/Spinner";
 import CreatePortfolioHeader from "../components/createportfolio/CreatePortfolioHeader";
 import CreatePortfolioTemplate from "../components/createportfolio/CreatePortfolioTemplate";
 
 export default function CreatePortfolioPage() {
-  const { data } = useQuery(["portfolios/self"], getPortfolioSelf);
+  const { isLoading, data } = useQuery(["portfolios/self"], getPortfolioSelf);
 
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <Spinner />
+      </div>
+    );
+  }
   return (
     <div className="w-full h-full">
       <CreatePortfolioHeader />
