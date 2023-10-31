fe2a6633ac7d3351f85c8b7d727d396076be7afb
kimchanho97 <nh0903@pusan.ac.kr>
2023-10-31 17:16:10 +0900

    chore: Remove console.log

--- src/hooks/usePreventGoBack.js
+++ src/hooks/usePreventGoBack.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 export default function usePreventGoBack() {
   const navigate = useNavigate();
   const preventGoBack = () => {
-    console.log("click", window.history);
     if (
       // eslint-disable-next-line no-restricted-globals
       confirm("페이지를 나가시겠습니까?\n변경사항이 저장되지 않을 수 있습니다.")
