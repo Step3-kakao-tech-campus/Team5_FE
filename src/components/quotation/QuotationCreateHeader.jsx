ec6e759755dd6a97aa30f830b646c99ef4d69e91
Hyunbin Lee <blackhblee@gmail.com>
2023-10-31 13:06:25 +0900

    feat: Implement quotation create page
    견적서 추가 페이지를 구현했습니다.

---
+++ src/components/quotation/QuotationCreateHeader.jsx
@@ -0,0 +1,11 @@
+import BackButtonHeader from "../common/BackButtonHeader";
+
+const QuotationCreateHeader = () => {
+  return (
+    <BackButtonHeader>
+      <span className="text-sm pl-10">견적서 추가</span>
+    </BackButtonHeader>
+  );
+};
+
+export default QuotationCreateHeader;
