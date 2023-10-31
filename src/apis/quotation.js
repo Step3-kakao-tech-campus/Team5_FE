014d5e6e11c6672c89d3b691e2aea4d6f4966105
Hyunbin Lee <blackhblee@gmail.com>
2023-10-31 13:05:38 +0900

    fix: Fix quotation api
    견적서 api를 수정했습니다.

--- src/apis/quotation.js
+++ src/apis/quotation.js
@@ -5,14 +5,27 @@ export const getQuotationList = async (chatId) => {
   return response.data;
 };
 
-export const createQuotation = async (chatId) => {
-  const response = await instance.post(`/quotations?chatId=${chatId}`);
+export const createQuotation = async (chatId, data) => {
+  const { title, company, description, price } = data;
+  const response = await instance.post(`/quotations?chatId=${chatId}`, {
+    title,
+    company,
+    description,
+    price,
+  });
   return response.data;
 };
 
-export const updateQuotation = async (quotationId, chatId) => {
+export const updateQuotation = async (quotationId, chatId, data) => {
+  const { title, company, description, price } = data;
   const response = await instance.put(
     `/quotations/${quotationId}?chatId=${chatId}`,
+    {
+      title,
+      company,
+      description,
+      price,
+    },
   );
   return response.data;
 };
@@ -24,7 +37,7 @@ export const confirmQuotationAll = async (chatId) => {
 
 export const confirmQuotationDetail = async (quotationId, chatId) => {
   const response = await instance.post(
-    `/quotations/confirm/${quotationId}/?chatId=${chatId}`,
+    `/quotations/confirm/${quotationId}?chatId=${chatId}`,
   );
   return response.data;
 };
