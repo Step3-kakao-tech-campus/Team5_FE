057dcbd176568033caea11574e4bd72a5165c537
Hyunbin Lee <blackhblee@gmail.com>
2023-11-02 01:10:13 +0900

    feat: Add quotation collect mock handler
    견적서 모아보기 mock handler를 추가했습니다.

--- src/mocks/quotationHandler.js
+++ src/mocks/quotationHandler.js
@@ -1,6 +1,11 @@
 import { rest } from "msw";
 import { sucess } from "./commonData";
-import { quotationList } from "./quotationData";
+import {
+  quotationCollectList1,
+  quotationCollectList2,
+  quotationCollectList3,
+  quotationList,
+} from "./quotationData";
 
 async function sleep(ms) {
   return new Promise((resolve) => {
@@ -82,5 +87,33 @@ export const quotationHandlers = [
       );
     }
     return res(ctx.status(200), ctx.json(sucess));
+  }),
+
+  rest.get("quotations/collect", async (req, res, ctx) => {
+    await sleep(500);
+    const accessToken = req.headers.get("Authorization");
+    const page = req.url.searchParams.get("page");
+    if (!accessToken) {
+      return res(
+        ctx.status(403),
+        ctx.json({
+          code: 403,
+          message: "Not authorized",
+        }),
+      );
+    }
+    if (page === "1") {
+      return res(ctx.status(200), ctx.json(quotationCollectList1));
+    }
+    if (page === "2") {
+      return res(ctx.status(200), ctx.json(quotationCollectList2));
+    }
+    if (page === "3") {
+      return res(ctx.status(200), ctx.json(quotationCollectList3));
+    }
+    return res(
+      ctx.status(200),
+      ctx.json({ success: true, response: [], error: null }),
+    );
   }),
 ];
