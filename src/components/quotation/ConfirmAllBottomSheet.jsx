446e2ec95f673c43d3a1f7c5f68c35fe29eda232
Hyunbin Lee <blackhblee@gmail.com>
2023-11-01 11:27:29 +0900

    feat: Add policy checkbox
    안내사항 동의 체크박스를 추가했습니다.

--- src/components/quotation/ConfirmAllBottomSheet.jsx
+++ src/components/quotation/ConfirmAllBottomSheet.jsx
@@ -1,9 +1,19 @@
+import React, { useState } from "react";
 import { confirmQuotationAll } from "../../apis/quotation";
 import BottomSheet from "../common/bottomsheet/BottomSheet";
 import Button from "../common/atoms/Button";
 
 const ConfirmAllBottomSheet = ({ onClose, chatId }) => {
+  const [agreePolicy, setAgreePolicy] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleAgreement = () => {
+    setAgreePolicy(!agreePolicy);
+  };
+
   const handleConfirmAll = async () => {
+    if (!agreePolicy) return;
+    setIsSubmitting(true);
     try {
       const response = await confirmQuotationAll(chatId);
       console.log(response);
@@ -13,6 +23,7 @@ const ConfirmAllBottomSheet = ({ onClose, chatId }) => {
     } catch (error) {
       console.log(error);
     }
+    setIsSubmitting(false);
   };
 
   return (
@@ -22,11 +33,27 @@ const ConfirmAllBottomSheet = ({ onClose, chatId }) => {
           <span>모든 결제가 완료되었나요?</span>
           <span>확정 후에는 취소가 불가능합니다.</span>
         </div>
+        <div className="pb-[15px]">
+          <label htmlFor="policy" className="flex gap-1 items-center px-1">
+            <input
+              type="checkbox"
+              id="policy"
+              name="policy-agree"
+              checked={agreePolicy}
+              onChange={handleAgreement}
+              className="w-[14px] h-[14px] rounded-[4px] border-lightgray-sunsu cursor-pointer accent-blue-sunsu"
+            />
+            <span className="text-xs">
+              안내사항을 확인하였으며, 이에 동의합니다.
+            </span>
+          </label>
+        </div>
         <Button
-          className="block w-full h-[50px] rounded-[10px] text-sm bg-lightskyblue-sunsu"
-          onClick={() => {
-            handleConfirmAll();
-          }}
+          className={`block w-full h-[50px] rounded-[10px] text-sm ${
+            agreePolicy ? "bg-lightskyblue-sunsu" : "bg-zinc-300"
+          }`}
+          onClick={handleConfirmAll}
+          disabled={isSubmitting}
         >
           모든 결제 확정하기
         </Button>
