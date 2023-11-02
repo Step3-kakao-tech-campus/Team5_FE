c363c00b5105b705da67f01ec98f819ec70cea62
Hyunbin Lee <blackhblee@gmail.com>
2023-11-03 02:06:33 +0900

    feat: Implement InProgressQuotationBanner
    진행 중인 견적서가 있을 때 표시되는 배너를 구현했습니다.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import MainCarousel from "../components/main/MainCarousel";
 import MainHeaderRow from "../components/main/MainHeaderRow";
 import MainSearchBar from "../components/main/MainSearchBar";
+import InProgressQuotationBanner from "../components/main/InProgressQuotationBanner";
 
 export default function MainPage() {
   const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
+  const { userInfo } = useSelector((state) => state.user);
 
   const handleOpenSearchBar = () => {
     setIsSearchBarOpen(true);
@@ -21,6 +24,7 @@ export default function MainPage() {
       {!isSearchBarOpen && (
         <MainHeaderRow handleOpenSearchBar={handleOpenSearchBar} />
       )}
+      {userInfo.role === "couple" && <InProgressQuotationBanner />}
       <MainCarousel />
     </div>
   );
