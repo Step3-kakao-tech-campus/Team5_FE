86888ce634093d4f6a7201eb81096c74e2c15b74
Hyunbin Lee <blackhblee@gmail.com>
2023-11-04 02:18:42 +0900

    feat: Add useFetchFavorites hook
    useFetchFavorites 커스텀훅을 추가했습니다.

---
+++ src/hooks/useFetchFavorites.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useInfiniteQuery } from "react-query";
+import _ from "lodash";
+import { getFavoriteList } from "../apis/favorite";
+
+const useFetchFavorites = () => {
+  const [favorites, setFavorites] = useState([]);
+  const infiniteQuery = useInfiniteQuery(
+    ["favorites"],
+    ({ pageParam = 0 }) => getFavoriteList(pageParam),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        if (lastPage.length < 10) {
+          return undefined;
+        }
+        return allPages.length;
+      },
+      keepPreviousData: true,
+    },
+  );
+
+  useEffect(() => {
+    if (infiniteQuery.data) {
+      const allFetchedFavorites = infiniteQuery.data?.pages.flat();
+      setFavorites((prev) =>
+        _.unionBy([...prev, ...allFetchedFavorites], "id"),
+      );
+    }
+  }, [infiniteQuery.data]);
+
+  return {
+    favorites,
+    ...infiniteQuery,
+  };
+};
+
+export default useFetchFavorites;
