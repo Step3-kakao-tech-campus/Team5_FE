db02772a67259d92404fc89e10c2fe9c4342ab41
kimchanho97 <nh0903@pusan.ac.kr>
2023-11-05 00:48:20 +0900

    design: Add overlay on FilterForm

--- src/components/portfolios/FilterForm.jsx
+++ src/components/portfolios/FilterForm.jsx
@@ -1,15 +1,41 @@
 import cn from "classnames";
-import React from "react";
+import React, { useRef } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { regions } from "../../utils/constants";
 import RangeSlider from "./RangeSlider";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 export default function FilterForm({
   selectedRegion,
   setSelectedRegion,
   prices,
   setPrices,
+  handleFilterForm,
+  setQueryLocation,
+  setQueryMinPrice,
+  setQueryMaxPrice,
 }) {
+  const filterFormRef = useRef(null);
+  const overlayRef = useRef(null);
+
+  const handleResetFilter = () => {
+    setSelectedRegion(null);
+    setPrices([0, 10_000_000]);
+  };
+  const handleApplyFilter = () => {
+    setQueryLocation(selectedRegion);
+    // eslint-disable-next-line prefer-destructuring
+    setQueryMinPrice(prices[0]);
+    if (prices[1] === 10_000_000) {
+      setQueryMaxPrice(null);
+      handleFilterForm();
+      return;
+    }
+    // eslint-disable-next-line prefer-destructuring
+    setQueryMaxPrice(prices[1]);
+    handleFilterForm();
+  };
+
   const handleRegionChange = (event) => {
     if (selectedRegion === event.target.value) {
       setSelectedRegion(null);
@@ -18,41 +44,67 @@ export default function FilterForm({
     setSelectedRegion(event.target.value);
   };
 
+  useOnClickOutside(filterFormRef, overlayRef, handleFilterForm);
+
   return (
     <>
-      <div>
-        <h4 className="text-xs text-gray-sunsu pb-1">지역</h4>
-        <div className=" grid grid-cols-2 text-[14px] gap-2">
-          {regions.map((region) => (
-            <label
-              htmlFor={region}
-              key={region}
-              className={cn(
-                " hover:underline cursor-pointer w-fit flex items-center",
-                {
-                  "text-blue-sunsu": selectedRegion === region,
-                },
-              )}
-            >
-              <input
-                id={region}
-                type="radio"
-                name="region"
-                value={region}
-                onClick={handleRegionChange}
-                className="w-0"
-              />
-              {region}
-              {selectedRegion === region && (
-                <AiOutlineClose size={14} className=" text-blue-sunsu" />
-              )}
-            </label>
-          ))}
+      <div
+        className="overlay w-full max-w-[576px] h-full bg-black fixed opacity-50 z-40 top-[50px]"
+        ref={overlayRef}
+      />
+      <div
+        className="flex flex-col p-4 gap-4 fixed top-[50px] max-w-[576px] w-full bg-white z-40"
+        ref={filterFormRef}
+      >
+        <div>
+          <h4 className="text-xs text-gray-sunsu pb-1">지역</h4>
+          <div className=" grid grid-cols-2 text-[14px] gap-2">
+            {regions.map((region) => (
+              <label
+                htmlFor={region}
+                key={region}
+                className={cn(
+                  " hover:underline cursor-pointer w-fit flex items-center",
+                  {
+                    "text-blue-sunsu": selectedRegion === region,
+                  },
+                )}
+              >
+                <input
+                  id={region}
+                  type="radio"
+                  name="region"
+                  value={region}
+                  onClick={handleRegionChange}
+                  className="w-0"
+                />
+                {region}
+                {selectedRegion === region && (
+                  <AiOutlineClose size={14} className=" text-blue-sunsu" />
+                )}
+              </label>
+            ))}
+          </div>
         </div>
-      </div>
-      <div>
-        <h4 className="text-xs text-gray-sunsu mb-1">가격</h4>
-        <RangeSlider prices={prices} setPrices={setPrices} />
+        <div>
+          <h4 className="text-xs text-gray-sunsu mb-1">가격</h4>
+          <RangeSlider prices={prices} setPrices={setPrices} />
+        </div>
+
+        <div className="w-full flex items-center gap-3">
+          <button
+            className=" bg-lightgray-sunsu rounded-[10px] w-full h-[32px]"
+            onClick={handleResetFilter}
+          >
+            필터 초기화
+          </button>
+          <button
+            className=" rounded-[10px] w-full h-[32px] text-white bg-blue-sunsu"
+            onClick={handleApplyFilter}
+          >
+            적용
+          </button>
+        </div>
       </div>
     </>
   );
--- src/components/portfolios/PortfolioTemplate.jsx
+++ src/components/portfolios/PortfolioTemplate.jsx
@@ -5,11 +5,11 @@ import useFetchPortfolios from "../../hooks/useFetchPortfolios";
 import { openSeverErrorBottomSheet } from "../../utils/handleBottomSheet";
 import Container from "../common/atoms/Container";
 import Spinner from "../common/atoms/Spinner";
+import EmptySearchResult from "./EmptySearchResult";
+import FilterForm from "./FilterForm";
 import PortfolioGrid from "./PortfolioGrid";
 import PortfolioSearchBar from "./PortfolioSearchBar";
 import SearchHeaderRow from "./SearchHeaderRow";
-import FilterForm from "./FilterForm";
-import EmptySearchResult from "./EmptySearchResult";
 
 const PortfolioTemplate = () => {
   const dispatch = useDispatch();
@@ -22,10 +22,10 @@ const PortfolioTemplate = () => {
   const [selectedRegion, setSelectedRegion] = useState(null);
   const [prices, setPrices] = useState([0, 10_000_000]);
 
-  const queryName = useRef(searchParams.get("name") || "");
-  const queryLocation = useRef(searchParams.get("location"));
-  const queryMinPrice = useRef(searchParams.get("minPrice"));
-  const queryMaxPrice = useRef(searchParams.get("maxPrice"));
+  const [queryName, setQueryName] = useState(searchParams.get("name") || "");
+  const [queryLocation, setQueryLocation] = useState(null);
+  const [queryMinPrice, setQueryMinPrice] = useState(null);
+  const [queryMaxPrice, setQueryMaxPrice] = useState(null);
 
   const {
     isFetchingNextPage, // 다음 페이지를 가져오는 요청이 진행 중인지 여부
@@ -36,10 +36,10 @@ const PortfolioTemplate = () => {
     portfolios,
     isFetching,
   } = useFetchPortfolios({
-    name: queryName.current,
-    location: queryLocation.current,
-    minPrice: queryMinPrice.current,
-    maxPrice: queryMaxPrice.current,
+    name: queryName,
+    location: queryLocation,
+    minPrice: queryMinPrice,
+    maxPrice: queryMaxPrice,
   });
 
   const handleOpenSearchBar = () => {
@@ -55,30 +55,12 @@ const PortfolioTemplate = () => {
     setIsFilterFormOpen((prev) => !prev);
   };
 
-  const handleResetFilter = () => {
-    setSelectedRegion(null);
-    setPrices([0, 10_000_000]);
-  };
-  const handleApplyFilter = () => {
-    queryLocation.current = selectedRegion;
-    // eslint-disable-next-line prefer-destructuring
-    queryMinPrice.current = prices[0];
-    if (prices[1] === 10_000_000) {
-      queryMaxPrice.current = null;
-      handleFilterForm();
-      return;
-    }
-    // eslint-disable-next-line prefer-destructuring
-    queryMaxPrice.current = prices[1];
-    handleFilterForm();
-  };
-
   const onKeyDownEnter = (e) => {
     // 한글만 두 번 입력되는 문제가 발생 -> 한글은 자음과 모음의 조합으로 한 음절이 만들어지기 때문에 조합문자이고, 영어는 조합문자가 아니다.
     if (e.isComposing || e.keyCode === 229) return;
     if (e.key === "Enter") {
+      setQueryName(name);
       handleCloseSearchBar();
-      queryName.current = name;
     }
   };
 
@@ -126,31 +108,16 @@ const PortfolioTemplate = () => {
         />
       )}
       {isFilterFormOpen && (
-        <>
-          <div className="overlay w-full max-w-[576px] h-full bg-black fixed opacity-50 z-40 top-[50px]" />
-          <div className="flex flex-col p-4 gap-4 fixed top-[50px] max-w-[576px] w-full bg-white z-40">
-            <FilterForm
-              selectedRegion={selectedRegion}
-              setSelectedRegion={setSelectedRegion}
-              prices={prices}
-              setPrices={setPrices}
-            />
-            <div className="w-full flex items-center gap-3">
-              <button
-                className=" bg-lightgray-sunsu rounded-[10px] w-full h-[32px]"
-                onClick={handleResetFilter}
-              >
-                필터 초기화
-              </button>
-              <button
-                className=" rounded-[10px] w-full h-[32px] text-white bg-blue-sunsu"
-                onClick={handleApplyFilter}
-              >
-                적용
-              </button>
-            </div>
-          </div>
-        </>
+        <FilterForm
+          selectedRegion={selectedRegion}
+          setSelectedRegion={setSelectedRegion}
+          prices={prices}
+          setPrices={setPrices}
+          handleFilterForm={handleFilterForm}
+          setQueryLocation={setQueryLocation}
+          setQueryMinPrice={setQueryMinPrice}
+          setQueryMaxPrice={setQueryMaxPrice}
+        />
       )}
       <Container>
         {portfolios.length === 0 ? (
