0f5ab7a10b3935000a757136da9164ef5e172e0d
kimchanho97 <nh0903@pusan.ac.kr>
2023-11-05 00:06:35 +0900

    feat: Implement ReviewListPage

---
+++ src/components/review/ReviewListHeader.jsx
@@ -0,0 +1,11 @@
+import BackButtonHeader from "../common/BackButtonHeader";
+
+export default function ReviewListHeader() {
+  return (
+    <BackButtonHeader>
+      <span className=" text-sm w-full text-center font-medium">
+        작성한 리뷰 목록
+      </span>
+    </BackButtonHeader>
+  );
+}
