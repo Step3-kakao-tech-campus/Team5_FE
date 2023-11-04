b4b8219db921f16ed6934256e510f40f166aacb1
kimchanho97 <nh0903@pusan.ac.kr>
2023-11-05 04:01:04 +0900

    feat: Create skeleton card

--- src/components/common/atoms/SkeletonCard.jsx
+++ src/components/common/atoms/SkeletonCard.jsx
@@ -1,16 +1,18 @@
+import "../skeleton.css";
+
 const SkeletonCard = () => {
   return (
     <div className="border-0">
       <div className="animate-pulse">
-        <div className="bg-lightgray-sunsu w-full after:pb-[100%] after:block" />
+        <div className="skeleton bg-lightgray-sunsu w-full after:pb-[100%] after:block" />
         <div className="py-[9px] px-[8px]">
           <div className="flex whitespace-nowrap">
-            <div className="inline bg-lightgray-sunsu h-[14px] w-[80px] mr-auto" />
-            <div className="inline bg-lightgray-sunsu h-[14px] w-[22px]" />
+            <div className="inline skeleton bg-lightgray-sunsu h-[14px] w-[80px] mr-auto" />
+            <div className="inline skeleton bg-lightgray-sunsu h-[14px] w-[22px]" />
           </div>
-          <div className="bg-lightgray-sunsu h-[31px] mt-[3px] w-full" />
-          <div className="bg-lightgray-sunsu h-[14px] mt-[3px] w-[70px]" />
-          <div className="bg-lightgray-sunsu h-[14px] mt-[3px] w-[45px]" />
+          <div className="skeleton bg-lightgray-sunsu h-[31px] mt-[3px] w-full" />
+          <div className="skeleton bg-lightgray-sunsu h-[14px] mt-[3px] w-[70px]" />
+          <div className="skeleton bg-lightgray-sunsu h-[14px] mt-[3px] w-[45px]" />
         </div>
       </div>
     </div>
