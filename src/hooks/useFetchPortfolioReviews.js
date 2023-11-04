001b61c110808485fdec5fd08d2d52ae80edc28e
kimchanho97 <nh0903@pusan.ac.kr>
2023-11-05 04:02:52 +0900

    feat: Implement custom hook fetchPortfolioReviews

---
+++ src/hooks/useFetchPortfolioReviews.js
@@ -0,0 +1,37 @@
+import _ from "lodash";
+import { useEffect, useState } from "react";
+import { useInfiniteQuery } from "react-query";
+import { getPortfolioReviews } from "../apis/review";
+
+const useFetchPortfolioReviews = () => {
+  const [portfolioReviews, setPortfolioReviews] = useState([]);
+  const infiniteQuery = useInfiniteQuery(
+    ["portfolio/reviews"],
+    ({ pageParam = 0 }) => getPortfolioReviews(pageParam),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        if (lastPage?.length < 10) {
+          return undefined;
+        }
+        return allPages.length;
+      },
+      keepPreviousData: true,
+    },
+  );
+
+  useEffect(() => {
+    if (infiniteQuery.data) {
+      const allFetchedFavorites = infiniteQuery.data?.pages.flat();
+      setPortfolioReviews((prev) =>
+        _.unionBy([...prev, ...allFetchedFavorites], "id"),
+      );
+    }
+  }, [infiniteQuery.data]);
+
+  return {
+    portfolioReviews,
+    ...infiniteQuery,
+  };
+};
+
+export default useFetchPortfolioReviews;
