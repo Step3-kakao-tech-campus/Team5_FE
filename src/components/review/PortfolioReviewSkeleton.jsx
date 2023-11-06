92d43f2ac2648e5c50a1a1f1a1745a14442e2b7f
kimchanho97 <nh0903@pusan.ac.kr>
2023-11-07 03:42:56 +0900

    design: Modify skeleton height

--- src/components/review/PortfolioReviewSkeleton.jsx
+++ src/components/review/PortfolioReviewSkeleton.jsx
@@ -4,12 +4,12 @@ import "../common/skeleton.css";
 export default function PortfolioReviewSkeleton() {
   return (
     <div className="flex flex-col gap-3 border-b-2 pb-5">
-      <div className="skeleton bg-lightgray-sunsu w-full after:pb-[35%] after:block rounded-[10px]" />
+      <div className="skeleton bg-lightgray-sunsu w-full after:pb-[75%] after:block " />
       <div className="flex justify-between">
-        <div className="skeleton bg-lightgray-sunsu w-[20%] after:pb-[15%] after:block rounded-[5px]" />
-        <div className="skeleton bg-lightgray-sunsu w-[10%] after:pb-[15%] after:block rounded-[5px]" />
+        <div className="skeleton bg-lightgray-sunsu w-[20%] after:pb-[15%] after:block " />
+        <div className="skeleton bg-lightgray-sunsu w-[10%] after:pb-[15%] after:block " />
       </div>
-      <div className="skeleton bg-lightgray-sunsu w-full after:pb-[15%] after:block rounded-[10px]" />
+      <div className="skeleton bg-lightgray-sunsu w-full after:pb-[20%] after:block " />
     </div>
   );
 }
