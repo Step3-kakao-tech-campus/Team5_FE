b8eec79b1679963471a9b2613552e2aacba7c6d3
kimchanho97 <nh0903@pusan.ac.kr>
2023-11-10 10:34:27 +0900

    refactor: Refactor ChatMessage

--- src/components/chat/ChatMessage.jsx
+++ src/components/chat/ChatMessage.jsx
@@ -3,31 +3,6 @@ import React from "react";
 import Photo from "../common/atoms/Photo";
 
 function ChatMessage({ message, isSender, counterAvatar }) {
-  const hasImageProperty = "image" in message;
-
-  if (hasImageProperty) {
-    return (
-      <div className={`flex gap-1  ${isSender ? "flex-row-reverse" : ""}`}>
-        {!isSender && (
-          <Photo
-            src={counterAvatar}
-            alt="avatar"
-            className="w-8 h-8 object-cover object-center rounded-full"
-          />
-        )}
-        <img
-          src={message.image}
-          alt="이미지"
-          className="max-w-[80%] max-h-[400px]"
-        />
-        <div className="flex flex-col self-end text-xs ">
-          <span>{message.isRead ? "" : 1}</span>
-          <span>{dayjs(message.timestamp).format("HH:mm")}</span>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className={`flex gap-1  ${isSender ? "flex-row-reverse" : ""}`}>
       {!isSender && (
@@ -37,13 +12,23 @@ function ChatMessage({ message, isSender, counterAvatar }) {
           className="w-8 h-8 object-cover object-center rounded-full"
         />
       )}
-      <div
-        className={`max-w-[70%] px-[14px] py-[10px] border border-lightgray-sunsu text-sm rounded-[20px] ${
-          isSender ? "bg-blue-sunsu text-white rounded-tr-sm" : "rounded-tl-sm"
-        } whitespace-pre-line`}
-      >
-        {message.content}
-      </div>
+      {message.image ? (
+        <img
+          src={message.image}
+          alt="이미지"
+          className="max-w-[70%] max-h-[400px]"
+        />
+      ) : (
+        <div
+          className={`max-w-[70%] px-[14px] py-[10px] border border-lightgray-sunsu text-sm rounded-[20px] ${
+            isSender
+              ? "bg-blue-sunsu text-white rounded-tr-sm"
+              : "rounded-tl-sm"
+          } whitespace-pre-line`}
+        >
+          {message.content}
+        </div>
+      )}
       <div
         className={`self-end text-xs flex flex-col ${
           isSender ? "items-end" : ""
